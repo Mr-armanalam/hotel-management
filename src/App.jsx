d26b1703fda6260d16700ae87ee5356969986698
Arman Alam <armanalam78578@gmail.com>
2025-07-03 05:23:32 +0530

    tailwind css and theme costmization are completed

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex h-screen text-center text-3xl font-Playfair font-bold'>App is the best</div>
+  )
+}
+
+export default App
