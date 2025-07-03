b4c3bca7a2190e1076b64054e93561da30cc3156
Arman Alam <armanalam78578@gmail.com>
2025-07-03 15:38:17 +0530

    service-type section completed in service page

--- src/modules/services/ui/services.jsx
+++ src/modules/services/ui/services.jsx
@@ -1,10 +1,12 @@
 import RoomTypes from '../section/room-types'
+import ServiceTypes from '../section/service-types'
 import ServicesHeader from '../section/services-header'
 
 const Services = () => {
   return (
     <>
       <ServicesHeader />
+      <ServiceTypes />
       <RoomTypes />
     </>
   )
