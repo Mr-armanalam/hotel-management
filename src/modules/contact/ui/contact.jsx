a023c6a2ca1b521892db6b261e39cc0bc7f32c26
Arman Alam <armanalam78578@gmail.com>
2025-07-04 19:52:23 +0530

    lazy loading added with services page

--- src/modules/contact/ui/contact.jsx
+++ src/modules/contact/ui/contact.jsx
@@ -1,10 +1,10 @@
 import { lazy, Suspense } from 'react'
-import IMap from '../section/map'
 import Loading from '../../loading'
 
 const Contact = () => {
   const ContactHeader = lazy(() => import('../section/contact-header'))
   const GetTouch = lazy(() => import('../section/get-touch'))
+  const IMap = lazy(() => import('../section/map'))
   return (
     <Suspense fallback={<Loading />}>
       <ContactHeader />
--- src/modules/services/ui/services.jsx
+++ src/modules/services/ui/services.jsx
@@ -1,9 +1,9 @@
 import { lazy, Suspense } from 'react'
-import ServiceTypes from '../section/service-types'
 import Loading from '../../loading'
 
 const Services = () => {
   const ServicesHeader = lazy(() => import('../section/services-header'));
+  const ServiceTypes = lazy(() => import('../section/service-types'))
   const RoomTypes = lazy(() => import('../section/room-types'));
   
   return (
